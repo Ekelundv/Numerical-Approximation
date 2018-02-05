46c56d51c2803719234c142a819c7563ba597226
Ekelundv <31965215+Ekelundv@users.noreply.github.com>
2018-02-05 20:49:03 +0100

    Almost working

---
+++ Task-3.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from numpy import *
+from pylab import *
+
+n = 3
+X = linspace(0, 2*pi, n+1)
+
+def f(x):
+    return cos(x)
+
+
+def divdiff(x):
+    L = []
+    L.append(list(f(x)))
+    for j in range(n):
+        C=[]
+        for i in range(0, n-j):
+            C.append((L[j][i+1]-L[j][i])/(X[j+i+1]-X[i]))
+        L.append(C)
+    return L
+
+
+def pn(X, x):
+    L = divdiff(X)
+    p = 0
+    for j in range(n+1):
+        xterm = 1
+        for i in range(j):
+            xterm = xterm*(x-X[i])
+        p = p + xterm*L[j][0]
+    return p
+
+print(divdiff(X))
+print(pn(X, x))
